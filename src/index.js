56f6ddeb524712a933a1d68f84b0ef78dea557e8
Sahil Dhumal <sahildhummal0104@gmail.com>
2025-12-11 16:28:26 +0530

    Update project structure, env setup, and MongoDB config

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+
+console.log("INDEX FILE EXECUTED");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    console.log("MongoDB Connected Successfully!");
+
+    app.get("/", (req, res) => {
+      res.send("Server & Database are running fine!");
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed:", error);
+  });
